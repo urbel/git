35c3af0d3b5d7e3c658b3fbd0273e225b4e24311
urbel <57218489+urbel@users.noreply.github.com>
2019-11-08 03:27:44 +0200

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,43 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Enter the number of rows for the tree and triangle: ");
+        int rows = scan.nextInt();
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + ", ");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i <= rows; i++) {
+
+
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (i * 2) - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = 0; i <= rows; i++) {
+
+
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            System.out.println();
+            for (int k = 1; k <= (i * 2) - 1; k++) {
+                System.out.print("*");
+            }
+        }
+    }
+}
